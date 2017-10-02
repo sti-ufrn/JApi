e660e9e0f7a18602a4c4086c56992885a637d3c7
Victor Oliveira <victorlopesjg@info.ufrn.br>
2017-10-02 11:54:50 -0300

    Atualizando

---
+++ app/build/generated/source/buildConfig/androidTest/debug/sinfo/ufrn/br/japi/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package sinfo.ufrn.br.japi.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "sinfo.ufrn.br.japi.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
